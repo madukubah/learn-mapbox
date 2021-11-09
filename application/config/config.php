f64b141f5b5f02ba299375dd03556a0fb927abb2
AlfalahM273 <MuhammadAlfalah.Madukubah@mitrais.com>
2021-11-10 07:50:53 +0800

    fix base url

--- application/config/config.php
+++ application/config/config.php
@@ -24,7 +24,7 @@ defined('BASEPATH') or exit('No direct script access allowed');
 |
 */
 // $config['base_url'] = 'http://localhost/gis-bombana/';
-$config['base_url'] = 'http://dev-gis.tenrimanagement.com/';
+$config['base_url'] = 'https://dev-gis.tenrimanagement.com/';
 
 /*
 |--------------------------------------------------------------------------
